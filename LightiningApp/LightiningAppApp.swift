6fa31c52d4a27beced64fb543d162598660e0f7c
Igor Gadomskii <igvgad@gmail.com>
2022-05-01 22:53:46 +0300

    Initial Commit

---
+++ LightiningApp/LightiningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightiningAppApp.swift
+//  LightiningApp
+//
+//  Created by Macbook on 01.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct LightiningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
